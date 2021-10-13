b3c66e595f3205f41acb35002e81cfa781f7d6cc
otsuneko <tkobayashi1021@gmail.com>
2021-10-13 19:47:04 +0900

    webdriverのoptions追加

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ ac_url = "https://atcoder.jp/users/" + user_name
 cf_url = "https://codeforces.com/profile/" + user_name
 
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--lang=ja-JP')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('start-maximized')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
 # AtCoder
